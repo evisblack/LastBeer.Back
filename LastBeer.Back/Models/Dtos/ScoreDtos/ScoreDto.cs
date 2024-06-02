dd6182f96e76aae483b248d41f1a6df840af34b4
EvisBlack <ediaziport@gmail.com>
2024-06-02 12:14:54 +0200

    Añadido Game a Scores

--- LastBeer.Back/Models/Dtos/ScoreDtos/ScoreDto.cs
+++ LastBeer.Back/Models/Dtos/ScoreDtos/ScoreDto.cs
@@ -5,6 +5,7 @@
         public int Id { get; set; }
         public int UserId { get; set; }
         public int GameId { get; set; }
+        public Game Game { get; set; }
         public int Points { get; set; }
         public DateTime ScoreDate { get; set; }
     }
